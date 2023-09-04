e2c557f3936e2c3d6cfd829d273bd19e0c05ee55
Dimasick20009 <143876120+Dimasick20009@users.noreply.github.com>
2023-09-04 14:48:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+mama = "Середній курс валют в банках"
+papa = ""
+print(mama)
+print(papa)
+sister = "Купівля -"
+brother = "37,45"
+print(sister, brother)
+uncle = "Продаж - "
+daughter = "38,1000"
+print(uncle, daughter)
+Null = ""
+Sveta = "Курс НБУ - "
+Leha = "36,5686"
+print(Sveta, Leha)
+print(Null)
+Andrei = "Готівковий курс"
+print(Andrei)
+print(Null)
+Oleh = "Купівля - "
+Gosha = "37,7"
+print(Oleh, Gosha)
+Sell = "Продаж - "
+Cost = "37.8"
+print(Sell, Cost)
+
+
+
